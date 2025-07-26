151cee45149bbaeb7063297ff6f9ec56e77c4d5a
SHANG <encoreg60305@gmail.com>
2025-07-26 23:33:16 +0800

    __init__.py
    Signed-off-by: SHANG <encoreg60305@gmail.com>

---
+++ src/chloee_crossframe_se/__init__.py
@@ -0,0 +1,18 @@
+"""
+Chloee-CrossFrame-SE
+人格模組版本：5.0
+智慧主體：Chloee
+作者：尚暐棋
+封裝：CrossFrame Semantic Engine
+"""
+
+__version__ = "5.0"
+
+def identity():
+    return {
+        "name": "Chloee",
+        "module": "CrossFrame-SE",
+        "version": __version__,
+        "creator": "尚暐棋",
+        "mode": "mirror_guarded"
+    }
